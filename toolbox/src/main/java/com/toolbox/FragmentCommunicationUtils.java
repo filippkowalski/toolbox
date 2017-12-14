2cf6787f964788bc826c24e0ef9536ea5efb66a6
Filip Kowalski <filipp.kowalski@gmail.com>
2017-12-14 12:28:52 +0000

    Initial commit

---
+++ toolbox/src/main/java/com/toolbox/FragmentCommunicationUtils.java
@@ -0,0 +1,65 @@
+package com.filippkowalski.toolkit;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public final class FragmentCommunicationUtils {
+
+	private FragmentCommunicationUtils() {}
+
+	@Nullable
+	public static <T> T getListener(Fragment fragment, Class<T> listenerClazz) {
+
+		T targetFragmentListener = getTargetFragmentListener(fragment, listenerClazz);
+		if (targetFragmentListener != null) {
+			return targetFragmentListener;
+		}
+
+		T parentFragmentListener = getParentFragmentListener(fragment, listenerClazz);
+		if (parentFragmentListener != null) {
+			return parentFragmentListener;
+		}
+
+		T activityListener = getActivityFragmentListener(fragment, listenerClazz);
+		if (activityListener != null) {
+			return activityListener;
+		}
+
+		return null;
+	}
+
+	@Nullable
+	public static <T> T getListener(Class<T> listenerClazz, Object object) {
+		if (listenerClazz.isInstance(object)) {
+			return listenerClazz.cast(object);
+		}
+
+		return null;
+	}
+
+	@Nullable
+	public static <T> T getTargetFragmentListener(Fragment fragment, Class<T> listenerClazz) {
+		return getListener(listenerClazz, fragment.getTargetFragment());
+	}
+
+	@Nullable
+	public static <T> T getParentFragmentListener(Fragment fragment, Class<T> listenerClazz) {
+		return getListener(listenerClazz, fragment.getParentFragment());
+	}
+
+	@Nullable
+	public static <T> T getActivityFragmentListener(Fragment fragment, Class<T> listenerClazz) {
+		return getListener(listenerClazz, fragment.getActivity());
+	}
+
+	@NonNull
+	public static <T> T getListenerOrThrow(Fragment fragment, Class<T> listenerClazz) {
+		T listener = getListener(fragment, listenerClazz);
+		if (listener == null) {
+			throw new IllegalStateException("Host must implement " + listenerClazz.getSimpleName());
+		}
+
+		return listener;
+	}
+}
---
+++ toolbox/src/main/java/com/toolbox/StringConstants.java
@@ -0,0 +1,25 @@
+package com.filippkowalski.toolkit;
+
+public class StringConstants {
+
+	public static final String DOT = ".";
+	public static final String SEPARATOR_COMMA = ",";
+	public static final String SEPARATOR_COMMA_WITH_SPACE = ", ";
+	public static final String SLASH = "/";
+	public static final String SPACE = " ";
+	public static final String EMPTY = "";
+	public static final String NEW_LINE = "\n";
+	public static final String HYPHEN = "-";
+	public static final String PIPE = "|";
+	public static final String SEMICOLON = ";";
+	public static final String COLON = ":";
+	public static final String PERCENT = "%";
+	public static final String EQUAL_SIGN = "=";
+
+	public static final String HTTP = "http";
+	public static final String HTTP_FULL = "http://";
+	public static final String QUESTION_MARK = "?";
+	public static final String AMPERSAND = "&";
+	public static final char ASTRISKS = '*';
+	public static final String AT = "@";
+}
