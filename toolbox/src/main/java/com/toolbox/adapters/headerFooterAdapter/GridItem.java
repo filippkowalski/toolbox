63daf576b1a7d008792b5f55abb04278058321bd
Filip Kowalski <filipp.kowalski@gmail.com>
2017-12-21 21:57:30 +0100

    Add MultiType adapter classes.

---
+++ toolbox/src/main/java/com/toolbox/adapters/headerFooterAdapter/GridItem.java
@@ -0,0 +1,8 @@
+package com.toolbox.adapters.headerFooterAdapter;
+
+/**
+ * Provides span when using multicolumn (grid, staggered) layout managers on RecyclerView
+ */
+public interface GridItem {
+    int getGridSpan();
+}
